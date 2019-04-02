2ed29c8e9ceffefc857f2a71e2f093652383e42c
Jayesh Kulkarni <kulkarniijayesh@gmail.com>
2019-04-02 21:43:27 +0530

    Added binary tree tutorials

---
+++ Tutorials/binary-tree.cs
@@ -0,0 +1,21 @@
+#Binary tree implementation
+
+using System;
+
+public class Node
+{
+	public int data;
+	public Node leftNode;
+	public Node rightNode;
+
+	public Node(int val)
+	{
+		data = val;
+		leftNode = null;
+		rightNode = null;
+	}
+
+}
+
+
+
