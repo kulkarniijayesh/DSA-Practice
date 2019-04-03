710b7fa890b9581af149ad2dd983d4e4c7979975
Jayesh Kulkarni <kulkarniijayesh@gmail.com>
2019-04-03 22:56:21 +0530

    Added code to insert nodes to BST and display tree in preorder

--- Tutorials/binary-tree.cs
+++ Tutorials/binary-tree.cs
@@ -17,5 +17,52 @@ public class Node
 
 }
 
+public class Tree
+{
+	public void insertNode(ref Node node, int val)
+	{
+		if(node == null)
+		{
+		    node = new Node(val);
+		}
+		else
+		{
+		    if(val < node.data)
+				insertNode(ref node.leftNode, val);
+			else if(val > node.data)
+				insertNode(ref node.rightNode, val);
+		}
+	}
+	
+	public void displayTree(Node node)
+	{
+		if(node == null)
+		{
+		    return;
+		}
+		
+		Console.WriteLine(node.data);
+		displayTree(node.leftNode);
+		displayTree(node.rightNode);
+	}
+}
 
-
+public class DefaultClass
+{
+	
+	public static void Main()
+	{
+	    Node RootNode = null;
+	    Tree tr = new Tree();
+	    
+		tr.insertNode(ref RootNode, 2);
+		tr.insertNode(ref RootNode, 1);
+		tr.insertNode(ref RootNode, 3);
+		tr.insertNode(ref RootNode, 4);
+		tr.insertNode(ref RootNode, 5);
+		
+		
+		tr.displayTree(RootNode);
+		Console.ReadLine();
+	}
+}
